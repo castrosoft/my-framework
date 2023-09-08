67b6909571dfd605e84cbfac16739c9566a84240
Pedro Castro <castro.fcefyn@gmail.com>
2023-09-08 14:41:45 -0300

    Add minor comments on the CucumberTestSuite

--- src/test/java/CucumberTestSuite.java
+++ src/test/java/CucumberTestSuite.java
@@ -5,6 +5,21 @@ import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
+
+/**
+ * @Suite: This annotation marks the class as a test suite. In Cucumber, a test suite is a collection of scenarios
+ * (test cases) that are executed together.
+ * @IncludeEngines: This annotation indicates that this test suite should include the Cucumber execution engines to run
+ * tests written in Gherkin language.
+ * @SelectClasspathResource: This annotation specifies the location of Cucumber feature files. In this case, the
+ * feature files are expected to be in the classpath under the directory "/features." Feature files are plain text files
+ * containing descriptions of test scenarios written in Gherkin language.
+ * @ConfigurationParameter: This annotation configures the Cucumber reporting generator. It is used to specify which
+ * reports should be generated and where they should be saved. In this case, it appears that a custom reporting
+ * generator named "SerenityReporterParallel" is being used, which generates reports in a specific format.
+ * The generated reports will be saved at the location "build/test-results/timeline.
+ */
+
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("/features")
